acfb4003440c758eba4c5c0e14960ec95a27d3d6
Muhammad Younis <myounis.psf@gmail.com>
2026-06-18 18:13:52 +0500

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,12 @@ const invoiceRoutes = require('./routes/invoices')
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    'https://invoice-app-psi-azure.vercel.app',
+    'http://localhost:5173'
+  ]
+}))
 app.use(express.json())
 
 // Routes
