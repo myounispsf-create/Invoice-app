525af6b97690ab25a560acc390c172ab3a5ea896
Muhammad Younis <myounis.psf@gmail.com>
2026-06-23 01:22:49 +0500

    fix invoices route name

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const { router: authRoutes } = require('./routes/auth')
-const invoiceRoutes = require('./routes/invoices')
+const invoiceRoutes = require('./routes/Invoices')
 const app = express()
 const PORT = process.env.PORT || 5000
 
